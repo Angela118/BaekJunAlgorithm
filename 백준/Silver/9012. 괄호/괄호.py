600c839b8e6cf0dcfdbf12c457dbdd866250d962
Angela <ayoon@ucdavis.edu>
2024-01-24 16:17:13 +0900

    [Silver IV] Title: 괄호, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,32 @@
+import sys
+
+def parenthesis_stack(parenthesis_string):
+    stack_list=[]
+
+    for i in range(len(parenthesis_string)):
+        if parenthesis_string[i] == '(':
+            stack_list.append(parenthesis_string[i])
+        elif parenthesis_string[i] == ')':
+            if not stack_list:
+                return 'NO'
+            else:
+                stack_list.pop()
+        
+
+    if not stack_list:
+        return 'YES'
+    else:
+        return 'NO'
+
+
+if __name__ == '__main__':
+    T = int(sys.stdin.readline())
+
+    for _ in range(T):
+        parenthesis_string = sys.stdin.readline()
+        result = parenthesis_stack(parenthesis_string)
+        print(result)
+
+    
+
+    
