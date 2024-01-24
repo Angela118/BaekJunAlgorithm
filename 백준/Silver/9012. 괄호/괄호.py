1ade79703b92ea7a4dd803326cb51396baeb727b
Angela <ayoon@ucdavis.edu>
2024-01-24 16:26:29 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,31 +1,24 @@
 import sys
 
-def parenthesis_stack(parenthesis_string):
-    stack_list=[]
-
-    for i in range(len(parenthesis_string)):
-        if parenthesis_string[i] == '(':
-            stack_list.append(parenthesis_string[i])
-        elif parenthesis_string[i] == ')':
-            if not stack_list:
-                return 'NO'
-            else:
-                stack_list.pop()
-        
-
-    if not stack_list:
-        return 'YES'
-    else:
-        return 'NO'
-
-
 if __name__ == '__main__':
     T = int(sys.stdin.readline())
 
     for _ in range(T):
         parenthesis_string = sys.stdin.readline()
-        result = parenthesis_stack(parenthesis_string)
-        print(result)
+        stack_count=0
+
+        for i in parenthesis_string:
+            if i == '(':
+                stack_count+=1
+            elif i == ')':
+                stack_count-=1
+                if stack_count<0:
+                    break
+
+        if stack_count == 0:
+            print('YES')
+        else:
+            print('NO')
 
     
 
