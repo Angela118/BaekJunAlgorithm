4679d0b114c4c0aa58dacc7ad40b080c6443cc78
Angela <ayoon@ucdavis.edu>
2024-01-25 16:17:42 +0900

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+if __name__ == '__main__':
+    testcase = int(sys.stdin.readline())
+
+    for _ in range(testcase):
+        N, M = map(int, sys.stdin.readline().split())
+        doc_priority = deque(map(int, sys.stdin.readline().split()))
+        # doc_priority = deque([(i,idx) for idx, i in enumerate(doc_priority)])   # index와 짝 지어주기
+        idx = deque(range(len(doc_priority)))
+        
+        turn=0
+        while doc_priority:
+            if doc_priority[0] == max(doc_priority):
+                turn+=1
+                if idx[0] == M:
+                    print(turn)
+                    break
+                else:
+                    doc_priority.popleft()
+                    idx.popleft()
+            else:
+                doc_priority.rotate(-1)
+                idx.rotate(-1)
