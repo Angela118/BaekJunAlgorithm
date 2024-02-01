84bea179a39c26232b1d6e7c6c0d15a6028946ce
Angela <ayoon@ucdavis.edu>
2024-02-01 17:19:41 +0900

    [Silver II] Title: 좌표 압축, Time: 1648 ms, Memory: 206008 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,10 +1,10 @@
 import sys
 
+
 if __name__ == '__main__':
     N = int(sys.stdin.readline())
     input_num = list(map(int, sys.stdin.readline().split()))
     sorted_num = sorted(list(set(input_num)))
-    index_list = str(range(len(sorted_num)))
 
     sorted_dict = {}
     ans=[]
