980005dca8327ffe2dbb35e3ce498dfafe7396f0
Angela <ayoon@ucdavis.edu>
2024-02-02 00:55:50 +0900

    [Silver IV] Title: 수 찾기, Time: 168 ms, Memory: 50540 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,26 +1,13 @@
 import sys
 
-def find_num(N_nums, target):
-    start, end = 0, len(N_nums)-1
-    while start <= end:
-        mid = (start+end)//2
-
-        if N_nums[mid] == target:
-            return 1
-        elif N_nums[mid] < target:
-            start = mid+1
-        else:
-            end = mid-1
-    return 0
-
-
 if __name__ == '__main__':
     N = int(sys.stdin.readline())
-    N_nums = list(map(int, sys.stdin.readline().split()))
+    N_nums = set(map(int, sys.stdin.readline().split()))
     M = int(sys.stdin.readline())
     M_nums = list(map(int, sys.stdin.readline().split()))
 
-    N_nums.sort()
-    
     for num in M_nums:
-        print(find_num(N_nums, num))
+        if num in N_nums:
+            print(1)
+        else:
+            print(0)
