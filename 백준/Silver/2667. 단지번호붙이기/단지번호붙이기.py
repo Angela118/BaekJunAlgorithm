a2df0c19f692049b93abf41614a8f179bb430911
Angela <ayoon@ucdavis.edu>
2024-02-08 18:15:20 +0900

    [Silver I] Title: 단지번호붙이기, Time: 44 ms, Memory: 31212 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,39 +1,25 @@
 import sys
-from collections import deque
-
 
 # 상 하 좌 우 (-1,0), (1,0), (0,-1), (0,1)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def bfs(graph, x,y):
+def dfs(graph, x,y):
     global count
-    queue = deque()
-
-    queue.append((x,y)) # 시작점(x, y)를 큐에 추가
     graph[x][y] = 0     # 방문 했음을 표시 (다시 방문하지 않기 위함. 중복방지)
     count+=1
 
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx<0 or nx>=N or ny<0 or ny>=N:  # 움직인 좌표가 범위를 벗어날 경우
-                continue
-
-            if graph[nx][ny] == 1:  # 움직인 좌표에 집이 있는 경우
-                graph[nx][ny] = 0   # 방문 했음을 표시
-                queue.append((nx, ny))
-                count+=1
-            
-    return count
-
-
+        if nx<0 or nx>=N or ny<0 or ny>=N:  # 움직인 좌표가 범위를 벗어날 경우
+            continue
+        if graph[nx][ny] == 1:  # 움직인 좌표에 집이 있는 경우, 다시 dfs 탐색
+            dfs(graph, nx, ny)
+    
+    
 
 if __name__ == '__main__':
     N = int(sys.stdin.readline())
@@ -46,7 +32,8 @@ if __name__ == '__main__':
         for y in range(N):
             if graph[x][y] == 1:
                 count=0
-                houses.append(bfs(graph, x, y))
+                dfs(graph, x, y)
+                houses.append(count)
 
     
     print(len(houses))
