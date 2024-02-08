e72ce137a2317d7dbd8a3e3ce70bf4e1556bca1d
Angela <ayoon@ucdavis.edu>
2024-02-08 18:14:50 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -7,21 +7,31 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def dfs(x,y):
+def bfs(graph, x,y):
     global count
-    graph[x][y] = 0     # 방문 했음을 표시
+    queue = deque()
+
+    queue.append((x,y)) # 시작점(x, y)를 큐에 추가
+    graph[x][y] = 0     # 방문 했음을 표시 (다시 방문하지 않기 위함. 중복방지)
     count+=1
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
 
-        if nx<0 or nx>=N or ny<0 or ny>=N:  # 움직인 좌표가 범위를 벗어날 경우
-            continue
-        if graph[nx][ny] == 1:  # 움직인 좌표에 집이 있는 경우, 다시 dfs 탐색
-            dfs(nx, ny)
-    
-    
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx<0 or nx>=N or ny<0 or ny>=N:  # 움직인 좌표가 범위를 벗어날 경우
+                continue
+
+            if graph[nx][ny] == 1:  # 움직인 좌표에 집이 있는 경우
+                graph[nx][ny] = 0   # 방문 했음을 표시
+                queue.append((nx, ny))
+                count+=1
+            
+    return count
 
 
 
@@ -32,12 +42,11 @@ if __name__ == '__main__':
 
     houses=[]
 
-    for x in range(N):
+    for x in range(N):      # 전체 그래프를 돌면서 집이 있는 곳만 dfs 실행
         for y in range(N):
             if graph[x][y] == 1:
                 count=0
-                dfs(x, y)
-                houses.append(count)
+                houses.append(bfs(graph, x, y))
 
     
     print(len(houses))
