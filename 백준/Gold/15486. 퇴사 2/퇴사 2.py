2868f4dd41c5422399c4d743a763075820df02e8
Angela <ayoon@ucdavis.edu>
2024-03-02 14:58:36 +0900

    [Gold V] Title: 퇴사 2, Time: 2516 ms, Memory: 97108 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,29 +1,11 @@
-import sys
+from sys import stdin
 
+N = int(stdin.readline())
+dp = [0]*(N+1)
+for i in range(1, N+1):
+    t, p = map(int, stdin.readline().split())
+    dp[i] = max(dp[i-1], dp[i])
+    if i+t<=N+1:
+        dp[i+t-1] = max(dp[i-1]+p, dp[i+t-1])
 
-
-# def dfs(x, y, visited, total_prob):
-    
-
-
-if __name__ == '__main__':
-    N = int(sys.stdin.readline())
-
-    T, P=[0], [0]
-    for _ in range(N):
-        t, p = map(int, sys.stdin.readline().split())
-        T.append(t)
-        P.append(p)
-
-    profit = [0 for _ in range(N+1)]
-    
-    for now in range(1, N+1):
-        profit[now] = max(profit[now-1], profit[now])    # 현재까지의 최대 이익을 이전까지의 최대 이익으로 갱신
-        finish_date = now + T[now] - 1  # 이번 상담의 끝나는 날짜 (당일 포함)
-        if finish_date <= N:  # 이번 상담이 퇴사일 전에 끝나는 경우 (이번 상담을 실시하는 경우)
-            # 이번 상담이 끝나는 날의 이익 = max(이전까지의 최대 이익+이번 상담으로 얻을 수 있는 이익, 끝나는 날짜의 최대 이익)
-            profit[finish_date] = max(profit[now-1]+P[now], profit[finish_date])
-        
-
-
-    print(profit[-1])
+print(dp[-1])
