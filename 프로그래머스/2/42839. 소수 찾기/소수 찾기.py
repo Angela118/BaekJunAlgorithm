575c480b0b25ad8650243aa4a791bfa472388ce5
Angela <ayoon@ucdavis.edu>
2024-03-22 23:51:57 +0900

    [level 2] Title: 소수 찾기, Time: 0.24 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,17 +1,7 @@
 from itertools import permutations
 
-def isPrime(n):
-    if n==1:
-        return False
-    else:
-        for i in range(2, int(n**0.5)+1):
-            if n%i==0:
-                return False
-            else:
-                return True
 
 def solution(numbers):
-    answer = []
     numbers = [n for n in numbers]  # numbers를 하나씩 자른다.
     
     # 모든 조합 찾기
@@ -21,25 +11,23 @@ def solution(numbers):
     
     
     # 중복되지 않는 int형 리스트로 바꾸기
-    per_list = ["".join(p) for p in per_nums]    
-    per_list = list(set(map(int, per_list)))
+    per_list = [int("".join(p)) for p in per_nums]    
+    per_list = list(set(per_list))
+    print(per_list)
     
-    
-    # for per in per_list:
-    #     if isPrime(per):
-    #         answer+=1
-    
-    for n in per_list:                            # 모든 int형 숫자에 대해 소수인지 판별
-        if n < 2:                                 # 2보다 작은 1,0의 경우 소수 아님
+    answer=0
+    for per in per_list:
+        if per<2:
             continue
-        check = True            
-        for i in range(2,int(n**0.5) + 1):        # n의 제곱근 보다 작은 숫자까지만 나눗셈
-            if n % i == 0:                        # 하나라도 나눠떨어진다면 소수 아님!
-                check = False
+        
+        flag=True
+        for i in range(2, int(per**0.5)+1):
+            if per%i == 0:
+                flag=False
                 break
-        if check:
-            answer.append(n)                      # 소수일경우 answer 배열에 추가
-
-    return len(set(answer))  
+                
+        if flag:
+            answer+=1
+            
     
-    # return answer
+    return answer
