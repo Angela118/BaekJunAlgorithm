90becf0668987e03c9f6a847f028487117b4c956
Angela <ayoon@ucdavis.edu>
2024-04-08 13:08:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 103.30 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,18 +1,6 @@
+import collections
+
 def solution(participant, completion):
-    dict_p={}
-    for p in participant:
-        if p not in dict_p:
-            dict_p[p]=1
-        else:
-            dict_p[p]+=1
+    answer = collections.Counter(participant) - collections.Counter(completion)
     
-    for c in completion:
-        if dict_p[c] != 0:
-            dict_p[c]-=1
-    
-    for key in dict_p.keys():
-        if dict_p[key] == 1:
-            return key
-    
-    answer = ''
-    return answer
+    return list(answer.keys())[0]
