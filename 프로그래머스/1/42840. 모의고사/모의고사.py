1bf05883d9eaee67e6c62d248564da2bd87eb6dc
Angela <ayoon@ucdavis.edu>
2024-04-08 22:36:09 +0900

    [level 1] Title: 모의고사, Time: 3.79 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,13 +3,13 @@ def solution(answers):
     ans1, ans2, ans3 = [1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]
     
     for i in range(len(answers)):
-        if answers[i] == ans1[i%5]:
+        if answers[i] == ans1[i%len(ans1)]:
             answer[0]+=1
         
-        if answers[i] == ans2[i%8]:
+        if answers[i] == ans2[i%len(ans2)]:
             answer[1]+=1
         
-        if answers[i] == ans3[i%10]:
+        if answers[i] == ans3[i%len(ans3)]:
             answer[2]+=1
         
     res=[]
