4fe97e3fa4f15930311fbc871577c5e44e3d1c75
Angela <ayoon@ucdavis.edu>
2024-04-10 12:21:44 +0900

    [level 2] Title: 전화번호 목록, Time: 85.30 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,9 @@
-from collections import Counter
-
 def solution(phone_book):
-    phone_dict = set(phone_book)
+        # 문자열 정렬    
+        phone_book.sort()    
         
-    for phone_num in phone_book:
-        num=""
-        for digit in phone_num:
-            num+=digit
-            if num in phone_dict and num != phone_num:
-                return False
-            
-    return True
+        for i in range(len(phone_book)-1):  # 같은 문자(숫자) 기준으로 정렬 됐기 때문에, 앞 뒤만 비교하면 됨!        
+            if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:            
+                return False 
+        return True
+        
